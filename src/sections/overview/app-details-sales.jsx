414b04ec5a6c9c853c248dd4b147ba758f84a865
Fede Cardoso <hernan.f.cardoso@gmail.com>
2024-08-23 14:55:26 -0300

    feat. add components

---
+++ src/sections/overview/app-details-sales.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+
+import { Card, Grid, Typography } from '@mui/material';
+
+export default function AppDetailsSales() {
+  return (
+    <Grid container spacing={4}>
+      <Grid item xs={12} sm={3} md={3}>
+        <Card
+          sx={{
+            px: 3,
+            py: 5,
+            borderRadius: 2,
+            width: '100%',
+            height: 220,
+          }}
+        >
+          <Typography variant="h5">Tickets Vendidos</Typography>
+          <Typography mt={2} variant="inherit">
+            Boletos: 2,000
+          </Typography>
+          <Typography variant="inherit">Abones: 1,000</Typography>
+          <Typography mt={2} variant="subtitle1">
+            TOTAL: 3,000/ 4,000
+          </Typography>
+        </Card>
+      </Grid>
+      <Grid item xs={12} sm={3} md={3}>
+        <Card
+          sx={{
+            px: 3,
+            py: 5,
+            borderRadius: 2,
+            width: '100%',
+            height: 220,
+          }}
+        >
+          <Typography variant="h5">Recaudación</Typography>
+          <Typography variant="h3">$45,320</Typography>
+        </Card>
+      </Grid>
+      <Grid item xs={12} sm={6} md={6}>
+        <Card
+          sx={{
+            px: 3,
+            py: 5,
+            borderRadius: 2,
+            width: '100%',
+            height: 220,
+          }}
+        >
+          <Grid container >
+            <Grid xs={12} sm={6} md={6}>
+              <Typography variant="h5">Ocupación</Typography>
+              <Typography mt={2} variant="inherit">
+                Boletos: 1,900
+              </Typography>
+              <Typography variant="inherit">Abones: 957</Typography>
+              <Typography mt={2} variant="subtitle1">
+                TOTAL: 2,857/ 4,000
+              </Typography>
+            </Grid>
+            <Grid
+              xs={12}
+              sm={6}
+              md={6}
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center',
+              }}
+            >
+              <Typography variant="h2">63%</Typography>
+            </Grid>
+          </Grid>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+}
--- src/sections/overview/view/app-view.jsx
+++ src/sections/overview/view/app-view.jsx
@@ -10,10 +10,10 @@ import AppTasks from '../app-tasks';
 import AppNewsUpdate from '../app-news-update';
 import AppDetailsEvent from '../app-detail-event';
 import AppFiltersEvent from '../app-filters-event';
+import AppDetailsSales from '../app-details-sales';
 import AppOrderTimeline from '../app-order-timeline';
 import AppCurrentVisits from '../app-current-visits';
 import AppWebsiteVisits from '../app-website-visits';
-import AppWidgetSummary from '../app-widget-summary';
 import AppTrafficBySite from '../app-traffic-by-site';
 import AppCurrentSubject from '../app-current-subject';
 import AppConversionRates from '../app-conversion-rates';
@@ -37,46 +37,61 @@ export default function AppView() {
       <Grid container sx={{ my: 4 }} > 
         <AppDetailsEvent />
       </Grid>
+
+      <Grid container sx={{ my: 2 }} > 
+        <AppDetailsSales />
+      </Grid>
       <Grid container spacing={3}>
-        <Grid xs={12} sm={6} md={6}>
-          <AppWidgetSummary
-            title="Weekly Sales"
-            total={714000}
-            color="success"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
-          />
-        </Grid>
 
-        <Grid xs={12} sm={6} md={6}>
-          <AppWidgetSummary
-            title="New Users"
-            total={1352831}
-            color="info"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />}
-          />
-        </Grid>
 
-        <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
-            title="Item Orders"
-            total={1723315}
-            color="warning"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
-          />
-        </Grid>
-
-        <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
-            title="Bug Reports"
-            total={234}
-            color="error"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
-          />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={8}>
+      <Grid xs={12} md={6}>
           <AppWebsiteVisits
-            title="Website Visits"
+            title="Entradas vendidas por tribunas"
+            subheader="(+43%) than last year"
+            chart={{
+              labels: [
+                '01/01/2003',
+                '02/01/2003',
+                '03/01/2003',
+                '04/01/2003',
+                '05/01/2003',
+                '06/01/2003',
+                '07/01/2003',
+                '08/01/2003',
+                '09/01/2003',
+                '10/01/2003',
+                '11/01/2003',
+              ],
+              series: [
+                {
+                  name: 'Team A',
+                  type: 'column',
+                  fill: 'solid',
+                  data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
+                },
+                {
+                  name: 'Team B',
+                  type: 'area',
+                  fill: 'gradient',
+                  data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
+                },
+                {
+                  name: 'Team C',
+                  type: 'line',
+                  fill: 'solid',
+                  data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
+                },
+              ],
+            }}
+          />
+        </Grid>
+
+
+
+
+        <Grid xs={12} md={6} >
+          <AppWebsiteVisits
+            title="Monto vendido por tribuna boletas"
             subheader="(+43%) than last year"
             chart={{
               labels: [
